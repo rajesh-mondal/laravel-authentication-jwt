67d2ce58c0145be93047cadeb9d6e5f1c5e7e103
Rajesh Mondal <rajesh01749@gmail.com>
2025-03-03 21:25:59 +0600

    Developed API endpoint for user registration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 
 class UserController extends Controller {
     function UserRegistration( Request $request ) {
 
+        try {
+            User::create( [
+                'firstName' => $request->input( 'firstName' ),
+                'lastName'  => $request->input( 'lastName' ),
+                'email'     => $request->input( 'email' ),
+                'mobile'    => $request->input( 'mobile' ),
+                'password'  => $request->input( 'password' ),
+            ] );
+
+            return response()->json( [
+                'status'  => 'success',
+                'message' => 'User Registration Successfully',
+            ] );
+
+        } catch ( Exception $e ) {
+            return response()->json( [
+                'status'  => 'failed',
+                'message' => 'User Registration Failed',
+            ] );
+        }
     }
 }
