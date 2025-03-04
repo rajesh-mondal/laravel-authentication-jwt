edb2bb8b41bbe3ffa66a17ebdea3af11999bb659
Rajesh Mondal <rajesh01749@gmail.com>
2025-03-04 21:34:09 +0600

    Implemented OTP verification and JWT token issuance for password reset (API Endpoint)

--- app/Helper/JWTToken.php
+++ app/Helper/JWTToken.php
@@ -19,6 +19,18 @@ class JWTToken {
         return JWT::encode( $payload, $key, 'HS256' );
     }
 
+    public static function CreateTokenForSetPassword( $userEmail ) {
+        $key = env( 'JWT_KEY' );
+        $payload = [
+            'iss'       => 'laravel-token',
+            'iat'       => time(),
+            'exp'       => time() + 60 * 5,
+            'userEmail' => $userEmail,
+        ];
+
+        return JWT::encode( $payload, $key, 'HS256' );
+    }
+
     function VerifyToken( $token ): string {
         try {
             $key = env( 'JWT_KEY' );
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,4 +75,28 @@ class UserController extends Controller {
             ] );
         }
     }
+
+    function VerifyOTP( Request $request ) {
+        $email = $request->input( 'email' );
+        $otp = $request->input( 'otp' );
+        $count = User::where( 'email', '=', $email )
+            ->where( 'otp', '=', $otp )->count();
+
+        if ( $count == 1 ) {
+            User::where( 'email', '=', $email )->update( ['otp' => '0'] );
+
+            $token = JWTToken::CreateTokenForSetPassword( $request->input( 'email' ) );
+            return response()->json( [
+                'status'  => 'success',
+                'message' => 'OTP Verification Successful',
+                'token'   => $token,
+            ] );
+
+        } else {
+            return response()->json( [
+                'status'  => 'failed',
+                'message' => 'unauthorized',
+            ] );
+        }
+    }
 }
