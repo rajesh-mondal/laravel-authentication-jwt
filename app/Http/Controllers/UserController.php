25e7b1e29b7b655fc00dbdcfb28f5981c27a0f2e
Rajesh Mondal <rajesh01749@gmail.com>
2025-03-04 17:16:02 +0600

    Implemented OTP email sending for password recovery (API Endpoint)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Helper\JWTToken;
+use App\Mail\OTPMail;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller {
     function UserRegistration( Request $request ) {
@@ -45,6 +47,27 @@ class UserController extends Controller {
                 'token'   => $token,
             ] );
 
+        } else {
+            return response()->json( [
+                'status'  => 'failed',
+                'message' => 'unauthorized',
+            ] );
+        }
+    }
+
+    function SendOTPCode( Request $request ) {
+        $email = $request->input( 'email' );
+        $otp = rand( 1000, 9999 );
+        $count = User::where( 'email', '=', $email )->count();
+
+        if ( $count == 1 ) {
+            Mail::to( $email )->send( new OTPMail( $otp ) );
+            User::where( 'email', '=', $email )->update( ['otp' => $otp] );
+
+            return response()->json( [
+                'status'  => 'success',
+                'message' => '4 digit OTP code has been send to your email',
+            ] );
         } else {
             return response()->json( [
                 'status'  => 'failed',
